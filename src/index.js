6b8701a371678e6a9a10b20732b18e0e696f8097
Minh Truong <minh.truong@bigin.vn>
2017-09-09 13:34:19 +0700

    - Update component path

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import BSwitch from './components/Switch';
+import FloatLabelInput from './components/FloatLabelInput';
+import Checkbox from './components/Checkbox';
+import Radio from './components/Radio';
+
+import './scss/b-component.scss'
+
+Vue.component('BSwitch', BSwitch);
+Vue.component('BFloatLabelInput', FloatLabelInput);
+Vue.component('BCheckbox', Checkbox);
+Vue.component('BRadio', Radio);
