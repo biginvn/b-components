77a4531d9f038ad7576d9bedfc2314a53b5f5c9c
Minh Truong <minh.truong@bigin.vn>
2017-09-09 22:40:44 +0700

    - Add select dropdown

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,31 +1,26 @@
 <template>
 	<div class="b-select">
 		<label :for="id">{{ label }}</label>
-		<select :name="name" :id="id" :disabled="disabled" @change="update()">
-			<option v-for="item in items" :value="item.value" :selected="isSelected(item.value)">{{ item.name }}</option>
+		<select :name="name" :id="id" :disabled="disabled" @change="update($event.target.value)" v-model="selected">
+			<option v-for="item in items" :value="item.value">{{ item.name }}</option>
 		</select>
 	</div>
 </template>
 <script>
 	export default {
-		data() {
+		data () {
 			return {
-				selected : ''
 			}
 		},
 		props : [ 'id', 'default', 'label', 'name', 'disabled', 'list', 'item-key', 'item-val', 'value'],
-		watch : {
-			value() {
-				this.updateWhenNotFound()
-			},
-			list () {
-				this.updateWhenNotFound()
-			}
-		},
-		mounted () {
-			this.updateWhenNotFound()
-		},
 		computed : {
+			selected : {
+				get () {
+					return this.value;
+				},
+				set(newValue) {
+				}
+			},
 			items() {
 				if (this.list == undefined || this.list == null || this.list.length == 0){
 					if (this.default != undefined && this.default != null ){
@@ -48,49 +43,16 @@
 			}
 		},
 		methods : {
-			update() {
-				let selected = this.$el.querySelector('select').value
-				this.$emit('input', selected)
-				this.selected = selected
+			update(val) {
+				this.$emit('input', val)
 			},
 			isSelected(v) {
-				var a = 1;
 				if(this.value == v){
 					this.selected = v
 					return true
 				}
 				return false
 			},
-			isFound() {
-				var found = false;
-				if (this.list == undefined || this.list == null || this.list.length == 0){
-					return found;
-				}
-
-				let items = [];
-				for(let i=0; i< this.list.length; i++){
-					let listItem = this.list[i]
-					let item = {
-						value : listItem[this.itemVal], 
-						name : listItem[this.itemKey]
-					}
-					if (item.value == this.value){
-						found = true
-						break
-					}
-				}
-
-				return found;
-			},
-			updateWhenNotFound() {
-				if (this.list == undefined || this.list == null || this.list.length == 0){
-					this.$el.querySelector('select').value=null
-				} else {
-					var founded = this.isFound()
-					if(!founded)
-						this.$el.querySelector('select').value=null
-				}
-			}
 		}
 
 	}
