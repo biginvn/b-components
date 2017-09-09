729d41792fa96c2fb5182bec135fd8c9e6fce398
Minh Truong <minh.truong@bigin.vn>
2017-09-09 13:27:18 +0700

    - Add radio button

---
+++ src/components/Radio.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="b-radio">
+		<input type="radio" :name="name" :id="id" ref="bRadio" class="radio__input" :disabled="disabled" :checked="isCheck" @change="update()">
+
+		<span class="radio__checkmark"></span>
+		<label :for="id">{{ label }}</label>
+	</div>
+</template>
+<script>
+	export default {
+		props : [ 'value', 'disabled', 'name', 'id', 'label' ],
+		computed : {
+			isCheck() {
+				if (this.value == this.$attrs.value)
+					return true;
+				else return false;
+			}
+		},
+		methods : {
+			update() {
+				if (this.$refs.bRadio.checked)
+					this.$emit('input', this.$attrs.value);
+
+			}
+		}
+
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import BSwitch from './components/Switch';
 import FloatLabelInput from './components/FloatLabelInput';
 import Checkbox from './components/Checkbox';
+import Radio from './components/Radio';
 
 import './scss/b-component.scss'
 
 Vue.component('BSwitch', BSwitch);
 Vue.component('BFloatLabelInput', FloatLabelInput);
 Vue.component('BCheckbox', Checkbox);
+Vue.component('BRadio', Radio);
