fd6f9de85b39fe24dea4d2994704f8903c5b0e53
Gia Tuan <tuan.trinh@bigin.vn>
2017-09-21 15:26:43 +0700

    Numberic Formatted

---
+++ src/components/NumbericFormatted.js
@@ -0,0 +1,43 @@
+export default{
+	props: ['value','currency','prefixes'],
+	mounted() {
+		this.blur(this.value);
+	},
+	watch: {
+		value() {
+			this.blur(this.value);
+		},
+		prefixes() {
+			this.blur(this.value);
+		},
+		currency() {
+			this.blur(this.value);
+		}
+	},
+	methods: {
+		focus(value){
+			value = parseFloat(value.replace(/[^\d\.]/g, ""));
+			this.$el.querySelector('input').value = isNaN(value)? 0 : value;
+		},
+		blur(value){
+			value = parseFloat(value.toString().replace(/[^\d\.]/g, "")) ;
+			this.$emit("input", value);
+			if (this.prefixes) {
+				if(this.$el.querySelector('input').value = isNaN(value)){
+					this.$el.querySelector('input').value = 0 + ' ' + this.currency;
+				}
+				else{
+					this.$el.querySelector('input').value = value.toLocaleString() + ' ' + this.currency;
+				}
+			}
+			else{
+				if(this.$el.querySelector('input').value = isNaN(value)){
+					this.$el.querySelector('input').value = this.currency + ' ' + 0;
+				}
+				else{
+					this.$el.querySelector('input').value = this.currency + ' ' + value.toLocaleString();
+				}
+			}
+		}
+	}
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 
 import BTextField from './themes/ios/TextField.vue'
 import BCheckBox from './themes/ios/CheckBox.vue'
@@ -7,7 +7,9 @@ import BSwitch from './themes/ios/Switch.vue'
 import BSelect from './themes/ios/Select.vue'
 import BTextarea from './themes/ios/Textarea.vue'
 import BRating from './themes/ios/Rating.vue'
-import BRatingGt from './themes/ios/rating-gt.vue'
+import BNumbericFormatted from './themes/ios/NumbericFormatted.vue'
+// import BSelectRating from './themes/ios/select-rating-gt.vue'
+
 
 Vue.component('BTextField', BTextField)
 Vue.component('BCheckBox', BCheckBox)
@@ -16,4 +18,7 @@ Vue.component('BSwitch', BSwitch)
 Vue.component('BSelect', BSelect)
 Vue.component('BTextarea', BTextarea)
 Vue.component('BRating', BRating)
-Vue.component('BRatingGt', BRatingGt)
+// Vue.component('BRatingGt', BRatingGt)
+// Vue.component('BSelectRating', BSelectRating)
+Vue.component('BRating', BRating)
+Vue.component('BNumbericFormatted', BNumbericFormatted)
