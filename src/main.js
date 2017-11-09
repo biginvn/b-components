1c72bb17128ac3a7a522ecea6e3676369b3181c2
DESKTOP-VOSE0KA\Dell <duan.nguyen@bigin.vn>
2017-11-09 10:20:16 +0700

    update

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ import BDateAndTime from './themes/ios/DateAndTime.vue'
 import BListUpLoaded from './themes/ios/ListUpLoaded.vue'
 import BTinymce from './themes/ios/TinyMCE.vue'
 import BInputTag from './themes/ios/Tag.vue'
+import BModal from './themes/ios/Modal.vue'
 
 Vue.component('BTextField', BTextField)
 Vue.component('BCheckBox', BCheckBox)
@@ -43,3 +44,4 @@ Vue.component('BListUpLoaded', BListUpLoaded)
 Vue.component('BDateAndTime', BDateAndTime)
 Vue.component('BTinymce', BTinymce)
 Vue.component('BInputTag', BInputTag)
+Vue.component('BModal', BModal)
