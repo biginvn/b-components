ce8037d760761143958c9595595f56617044f569
Gia Tuan <tuan.trinh@bigin.vn>
2018-01-15 18:39:10 +0700

    Fix Confilct

--- built/b-components.js
+++ built/b-components.js
@@ -43313,14 +43313,16 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             this.mask = this.value;
         },
         blur(mask) {
-            if (mask == undefined) return;
+
+            // if(mask==undefined) return
             // Validation type Affix
             this.affix == '$' || this.affix == '€' ? mask : mask = Math.trunc(mask).toString();
             if (this.affix == '%') {
                 mask > 100 ? mask = '100' : mask;
             }
             // Get String position
-            var pos = mask.indexOf('.');
+            mask != null ? mask.indexOf('.') : mask = '';
+            var pos = mask;
             // Remove A-Z text
             mask = mask.toString().replace(/[^\d\.]/g, "");
             // Cut String to Forward & Behind  "432.11" => "432" & "11"
