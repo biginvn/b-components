099c74a421b51e7fcdbc6919e6eb93c649115570
AnhPham <anh.pham@bigin.vn>
2018-03-05 14:48:20 +0700

    add slot TextField

--- src/themes/ios/TextField.vue
+++ src/themes/ios/TextField.vue
@@ -2,6 +2,7 @@
  <div class="b__components b-ios b-float-label">
   <label :class="classLabel">{{ label }}</label>
   <input :placeholder="placeholder" :type="typeComponent" :min="min" :maxlength="maxLength" ref="bInput" :name="name" :id="id" :class="classes" :value="value" :disabled="disabled" @input="change($event.target.value)" @blur="blur" @focus="focus">
+  <slot name="otherElements"></slot>
  </div>
 </template>
 <script>
