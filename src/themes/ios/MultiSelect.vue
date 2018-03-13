39ec830ab557c121f7e4e9b98d98e0fe8e9a81de
hoangtt2807 <hoangtt2807@gmail.com>
2018-03-13 17:31:36 +0700

    add required for multiselect

--- src/themes/ios/MultiSelect.vue
+++ src/themes/ios/MultiSelect.vue
@@ -19,6 +19,9 @@
 				@keydown.40="keypressAction('ArrowDown')" @keydown.8="keypressAction('BackSpace')"
 				@keydown.38="keypressAction('ArrowUp')" @keydown.13="searchList.length > 0 && pointerIndex!=null ? toggleItem(searchList[pointerIndex].id) : ''"
 				class="input-control" @focus = "focusInputAction($event.target.value)" @input = "searchAction($event.target.value)" :value = "searchKeyword"
+				required
+				oninvalid="this.setCustomValidity('The query field must not be blank')"
+    			oninput="setCustomValidity('')"
 			></div>
 
 			<div class="control" @click="toggleList()">
