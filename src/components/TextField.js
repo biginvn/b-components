115df285a33ef1f44ad075e557579337bc12af38
hoangtt2807 <hoangtt2807@gmail.com>
2018-03-15 17:58:14 +0700

    edit multiselect and textfied

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,7 +2,7 @@ import baseComponent from '../mixins/text-field-mixins'
 
 export default {
 	mixins : [baseComponent],
-	props	: ['type', 'min', 'maxlength', 'classParent'],
+	props	: ['type', 'min', 'maxlength'],
 	computed : {
 		classes () {
 			return (this.className?this.className:'') + " b__input 2"
@@ -11,9 +11,6 @@ export default {
 			if (this.type == undefined || this.type== null || this.type.length == 0)
 				return 'text'
 			return this.type
-		},
-		classesParent() {
-			return (this.classParent?this.classParent:'')
 		}
 	},
 	methods : {
@@ -27,13 +24,20 @@ export default {
 		focus() {
 			this.$emit('focus')
 		},
-		keyup(value){
-			this.$emit('keyup', value)
-		},
+		// keyup(value){
+		// 	this.$emit('keyup', value)
+		// },
 		maxLength(){
 		   if (this.maxlength == undefined || this.maxlength== null)
 		    return
 		   return this.maxlength
-		}
+		},
+		keyupEnter: function(e) {
+			 var keyCode = e.keyCode || e.which;
+     		 if (keyCode === 13) {
+     		   e.preventDefault();
+     		   return false;
+     		 }   
+   		}
 	}
 }
