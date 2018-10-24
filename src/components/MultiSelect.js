ae1fda77d11163dced26545deac4a35175fbdf4c
AnhPhamBigin <anh.pham@bigin.vn>
2018-10-24 15:11:20 +0700

    fix multiselect

--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -1,39 +1,39 @@
 import baseComponent from '../mixins/base-mixins'
 
 export default {
-	data () {
-		return {
-			isExpanding : false,
-			searchList : [],
-			pointerIndex : null, // Selecting index of list
-			hoverIndex : null, // Position of cursor is hovering select item
-			searchKeyword : '',
-			isActive : false,
-		}
-	},
-	mixins: [baseComponent],
-	created () {
-		this.searchList = this.list
-		this.setStatus();
-	},
-	watch:{
-		list(newList) {
-			this.searchList = JSON.parse(JSON.stringify(newList))
-		},
-		resetSearchKeyWord() {
-			if (this.resetSearchKeyWord) {
-				this.searchKeyword = ''
-				this.$emit('search-keywords', '');
-			}
-		},
-		value(val){
-			if(val != "" && val != null)
-				this.isActive = true
-			else this.isActive = false
-		}
+    data () {
+        return {
+            isExpanding : false,
+            searchList : [],
+            pointerIndex : null, // Selecting index of list
+            hoverIndex : null, // Position of cursor is hovering select item
+            searchKeyword : '',
+            isActive : false,
+        }
+    },
+    mixins: [baseComponent],
+    created () {
+        this.searchList = this.list
+        this.setStatus();
+    },
+    watch:{
+        list(newList) {
+            this.searchList = JSON.parse(JSON.stringify(newList))
+        },
+        resetSearchKeyWord() {
+            if (this.resetSearchKeyWord) {
+                this.searchKeyword = ''
+                this.$emit('search-keywords', '');
+            }
+        },
+        value(val){
+            if(val != "" && val != null)
+                this.isActive = true
+            else this.isActive = false
+        }
 
-	},
-	props: {
+    },
+    props: {
         list: {
 
         },
@@ -58,198 +58,199 @@ export default {
 
         },
         resetSearchKeyWord: {
-        	type: Boolean,
-        	default: false
+            type: Boolean,
+            default: false
         }
     },
-	// props : ['list', 'value', 'disabled', 'single-dropdown', 'disable-icon', 'placeholder'],
-	computed : {
-		selected () { // Convert v-model to [] if it's null
-			return this.value ? this.value : (this.isSingle ? null : [])
-		},
-		isSingle(){
-			return this.singleDropdown === "true" ? true : false
-		},
-		listClasses () {
-			return (this.isExpanding ? "active" : "") + " b__multi__select__list"
-		}
-	},
-	methods : {
-		setStatus(){
-			if(this.value != "" && this.value != null)
-				this.isActive = true
-			else {
-				this.isActive = false
-			}
-		},
-		editQuery()
-		{
-			let self = this;
-			var getValue = this.value
-			this.list.filter(function(index) {
-				if (index.id == getValue) {
-					return self.searchKeyword = index.thumbHtml
-				}
-			})
+    // props : ['list', 'value', 'disabled', 'single-dropdown', 'disable-icon', 'placeholder'],
+    computed : {
+        selected () { // Convert v-model to [] if it's null
+            return this.value ? this.value : (this.isSingle ? null : [])
+        },
+        isSingle(){
+            return this.singleDropdown === "true" ? true : false
+        },
+        listClasses () {
+            return (this.isExpanding ? "active" : "") + " b__multi__select__list"
+        }
+    },
+    methods : {
+        setStatus(){
+            if(this.value != "" && this.value != null)
+                this.isActive = true
+            else {
+                this.isActive = false
+            }
+        },
+        editQuery()
+        {
+            let self = this;
+            var getValue = this.value
+            this.list.filter(function(index) {
+                if (index.id == getValue) {
+                    return self.searchKeyword = index.thumbHtml
+                }
+            })
 
-			this.$emit('input', null)
-			this.$nextTick(()=>{
-				if(this.$refs.inputSearch)
-					this.$refs.inputSearch.focus();
-			})
-		},
+            this.$emit('input', null)
+            this.$nextTick(()=>{
+                if(this.$refs.inputSearch)
+                    this.$refs.inputSearch.focus();
+            })
+        },
 
-		filterQuerylist(){
-			this.list.filter(function(index, data) {
-				if (data.id == this.value)
-				return data.thumbHtml
-			})
-		},
+        filterQuerylist(){
+            this.list.filter(function(index, data) {
+                if (data.id == this.value)
+                    return data.thumbHtml
+            })
+        },
 
-		closeDropdow(event){
-			this.switchList(false);
-			if(!this.value)
-				this.searchKeyword = null
-			if(this.searchList.length == 0){
-				this.isExpanding = false
-			}
-		},
+        closeDropdow(event){
+            console.log('close dropdown')
+            this.switchList(false);
+            if(!this.value)
+                this.searchKeyword = null
+            if(this.searchList.length == 0){
+                this.isExpanding = false
+            }
+        },
 
-		blurSearch(){
-			if(this.searchList.length == 0){
-				this.searchKeyword = ""
-			}
-		},
+        blurSearch(){
+            if(this.searchList.length == 0){
+                this.searchKeyword = ""
+            }
+        },
 
-		getSingleSelected(){
+        getSingleSelected(){
 
-			if(this.list == undefined || this.list == null)
-				return;
-			if(Array.isArray(this.list))
-			{
-				let listSelected = this.list.filter( (item) => {
-					return item.id == this.selected
-				})
+            if(this.list == undefined || this.list == null)
+                return;
+            if(Array.isArray(this.list))
+            {
+                let listSelected = this.list.filter( (item) => {
+                    return item.id == this.selected
+                })
 
-				if (listSelected.length > 0)
-					return listSelected[0]
-			}
-			return;
-		},
+                if (listSelected.length > 0)
+                    return listSelected[0]
+            }
+            return;
+        },
 
-		getSelectedList () { // Get selected with full information [ { id : .. , html : ... } ]
-			if (this.isSingle) return
-			let selected = []
-			this.selected.forEach( (id, index) => {
-				let item = this.list.find((value) => value.id == id)
-				if (item != undefined)
-					selected.push(item)
-			})
-        	return selected
-		},
+        getSelectedList () { // Get selected with full information [ { id : .. , html : ... } ]
+            if (this.isSingle) return
+            let selected = []
+            this.selected.forEach( (id, index) => {
+                let item = this.list.find((value) => value.id == id)
+                if (item != undefined)
+                    selected.push(item)
+            })
+            return selected
+        },
 
-		toggleList (e) {
+        toggleList (e) {
             e.stopPropagation();
             this.switchList(!this.isExpanding);
-		},
+        },
 
-		switchList (on = true) {
-			if (on)
-				this.isExpanding = true
-			else
-				this.isExpanding = false
-		},
+        switchList (on = true) {
+            if (on)
+                this.isExpanding = true
+            else
+                this.isExpanding = false
+        },
 
-		toggleItem(id){
-			if (!this.isSingle){
-				let selectList = this.value == null ? [] : this.value;
-				if (selectList.includes(id))
-					selectList.splice(selectList.indexOf(id), 1)
-				else
-					selectList.push(id)
+        toggleItem(id){
+            if (!this.isSingle){
+                let selectList = this.value == null ? [] : this.value;
+                if (selectList.includes(id))
+                    selectList.splice(selectList.indexOf(id), 1)
+                else
+                    selectList.push(id)
 
-				this.switchList(true)
+                this.switchList(true)
 
-				// Reset search keyword at input field
-				this.searchKeyword = ''
-				this.$el.querySelector('input.input-control').focus()
-				this.focusInputAction('')
+                // Reset search keyword at input field
+                this.searchKeyword = ''
+                this.$el.querySelector('input.input-control').focus()
+                this.focusInputAction('')
 
-				this.$emit('input', selectList)
-			} else {
-				let selectList = this.value == null ? [] : [this.value];
-				if (selectList.includes(id)){
-					selectList.splice(selectList.indexOf(id), 1)
-					this.$emit('input', null)
-					if(this.$el.querySelector('input.input-control') != null){
-						this.$el.querySelector('input.input-control').focus()
-					}
-				}
-				else{
-					selectList = [id]
-					this.$emit('input', id)
-				}
-				
-				// Reset search keyword at input field
-				this.searchKeyword = ''
-				this.focusInputAction('')
+                this.$emit('input', selectList)
+            } else {
+                let selectList = this.value == null ? [] : [this.value];
+                if (selectList.includes(id)){
+                    selectList.splice(selectList.indexOf(id), 1)
+                    this.$emit('input', null)
+                    if(this.$el.querySelector('input.input-control') != null){
+                        this.$el.querySelector('input.input-control').focus()
+                    }
+                }
+                else{
+                    selectList = [id]
+                    this.$emit('input', id)
+                }
 
-				this.$nextTick(()=>{
-					this.switchList(false)
-				})
-			}
-		},
-		hoverItem(index){ // Hover on item at (index) in searchList
-			// this
-		},
+                // Reset search keyword at input field
+                this.searchKeyword = ''
+                this.focusInputAction('')
 
-		searchAction (keyword) {
-			this.searchKeyword = keyword
-			this.switchList(true)
-			this.$emit('search-keywords', keyword);
-			if(keyword == undefined && keyword == null || keyword.length == 0) {
-				this.searchList = JSON.parse(JSON.stringify(this.list))
-				return
-			}
-			this.searchList = this.list.filter( (item, position) => {
-				if (item.keywords == undefined || item.keywords == null)
-					return false
-				let regex = new RegExp('.*' + keyword.toLowerCase() +'.*')
-				return item.keywords.toLowerCase().match(regex)
-			})
-		},
-		focusInputAction (keyword) {
-			this.searchAction(keyword)
-			this.switchList(true)
-		},
-		keypressAction (keyName){
-			let pointerIndex = this.pointerIndex
-			switch (keyName) {
-				case 'ArrowDown':
-				if (this.pointerIndex == null || this.pointerIndex >= this.searchList.length - 1){
-					pointerIndex = 0
-					break
-				}
+                this.$nextTick(()=>{
+                    this.switchList(false)
+                })
+            }
+        },
+        hoverItem(index){ // Hover on item at (index) in searchList
+            // this
+        },
 
-				pointerIndex++
-				break
-				case 'ArrowUp':
-				if (this.pointerIndex == null || this.pointerIndex == 0){
-					pointerIndex = this.searchList.length-1
-					break
-				}
+        searchAction (keyword) {
+            this.searchKeyword = keyword
+            this.switchList(true)
+            this.$emit('search-keywords', keyword);
+            if(keyword == undefined && keyword == null || keyword.length == 0) {
+                this.searchList = JSON.parse(JSON.stringify(this.list))
+                return
+            }
+            this.searchList = this.list.filter( (item, position) => {
+                if (item.keywords == undefined || item.keywords == null)
+                    return false
+                let regex = new RegExp('.*' + keyword.toLowerCase() +'.*')
+                return item.keywords.toLowerCase().match(regex)
+            })
+        },
+        focusInputAction (keyword) {
+            this.searchAction(keyword)
+            this.switchList(true)
+        },
+        keypressAction (keyName){
+            let pointerIndex = this.pointerIndex
+            switch (keyName) {
+                case 'ArrowDown':
+                    if (this.pointerIndex == null || this.pointerIndex >= this.searchList.length - 1){
+                        pointerIndex = 0
+                        break
+                    }
 
-				pointerIndex--
-				break
-				case 'BackSpace':
-				pointerIndex = null
-				if (this.value != null && this.value.length > 0 && this.searchKeyword.length == 0)
-					this.value.splice(this.value.length-1,1)
-			}
+                    pointerIndex++
+                    break
+                case 'ArrowUp':
+                    if (this.pointerIndex == null || this.pointerIndex == 0){
+                        pointerIndex = this.searchList.length-1
+                        break
+                    }
 
-			this.hoverItem(pointerIndex)
-			this.pointerIndex = pointerIndex
-		}
-	}
+                    pointerIndex--
+                    break
+                case 'BackSpace':
+                    pointerIndex = null
+                    if (this.value != null && this.value.length > 0 && this.searchKeyword.length == 0)
+                        this.value.splice(this.value.length-1,1)
+            }
+
+            this.hoverItem(pointerIndex)
+            this.pointerIndex = pointerIndex
+        }
+    }
 
 }
--- src/themes/ios/MultiSelect.vue
+++ src/themes/ios/MultiSelect.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="b__components b__multi__select" @mouseleave = "switchList(false)" @click = "switchList(true)">
+	<div class="b__components b__multi__select" tabindex="0" @blur = "closeDropdow()" @click = "switchList(true)">
 		<label :for="id" :class="isActive ? 'active' : '' ">{{ label }}</label>
 		<div class="b__multi__select__control" v-bind:class="{ addBorder : isExpanding, multi: !singleDropdown }">
 			<div class="selected" v-if="!isSingle" v-for="item in getSelectedList()">
@@ -8,38 +8,37 @@
 			</div>
 
 			<div class="selected single" v-if="isSingle">
-				<span 
-					class="thumb" 
-					v-if="getSingleSelected()!=null"
-					v-html="getSingleSelected().thumbHtml"
-					@click='editQuery()'	
+				<span
+						class="thumb"
+						v-if="getSingleSelected()!=null"
+						v-html="getSingleSelected().thumbHtml"
+						@click='editQuery()'
 				>
 				</span>
 			</div>
 
 			<div class="input-control-wrap" v-if = "!isSingle || getSingleSelected() == null " style="width:100%;">
 				<input
-				v-show="singleDropdown"
-				:placeholder="placeholder"
-				type="text" 
-				style="font-family: 'Open Sans',sans-serif; font-size: 14px; position: absolute; top: 5px; left: 10px; width: 90%;" 
-				@keydown.40="keypressAction('ArrowDown')" @keydown.8="keypressAction('BackSpace')"
-				@keydown.38="keypressAction('ArrowUp')" @keydown.13="searchList.length > 0 && pointerIndex!=null ? toggleItem(searchList[pointerIndex].id) : ''"
-				class="input-control" @focus="focusInputAction($event.target.value);$emit('removeRequired')" @input = "searchAction($event.target.value)" :value = "searchKeyword"
-    			@blur='closeDropdow()'
-    			onClick="this.select()"
-			></div>
+						ref="inputSearch"
+						v-show="singleDropdown"
+						:placeholder="placeholder"
+						type="text"
+						style="font-family: 'Open Sans',sans-serif; font-size: 14px; position: absolute; top: 5px; left: 10px; width: 90%;"
+						@keydown.40="keypressAction('ArrowDown')" @keydown.8="keypressAction('BackSpace')"
+						@keydown.38="keypressAction('ArrowUp')" @keydown.13="searchList.length > 0 && pointerIndex!=null ? toggleItem(searchList[pointerIndex].id) : ''"
+						class="input-control" @focus="focusInputAction($event.target.value);$emit('removeRequired')" @input = "searchAction($event.target.value)" :value = "searchKeyword"
+						@blur='closeDropdow()'
+						onClick="this.select()"
+				></div>
 
-			<div :class="isExpanding ? 'iconC iconD' : 'iconC'" @click="toggleList()">
-				<!-- <i class="fa fa-angle-down iconC" aria-hidden="true" v-show="!isExpanding"></i>
-				<i class="fa fa-angle-up iconC" aria-hidden="true" v-show="isExpanding"></i> -->
+			<div :class="isExpanding ? 'iconC iconD' : 'iconC'" @click="toggleList($event)">
 			</div>
 		</div>
-		
+
 		<!-- <input type="hidden" :name="name" :value="value" class="mutiple-select-hidden-value"> -->
 		<ul v-bind:class="[{addBorder : isExpanding}, listClasses]">
 			<li v-show = "searchList == undefined || searchList.length == 0" class="not-found">Not found</li>
-			<li class="list-item" :class="{ 'active' : (!isSingle && selected.includes(item.id)) || ( isSingle && selected == item.id ) , 'hover' : index == pointerIndex }" v-for = "(item, index) in searchList" @click="toggleItem(item.id)">
+			<li class="list-item" :class="{ 'active' : (!isSingle && selected.includes(item.id)) || ( isSingle && selected == item.id ) , 'hover' : index == pointerIndex }" v-for = "(item, index) in searchList" @mousedown="toggleItem(item.id)">
 				<div class="icon" v-if = "!disableIcon">
 					<img :src="item.icon" class="icon-img">
 				</div>
@@ -49,8 +48,8 @@
 	</div>
 </template>
 <script>
-	import MultiSelect from './../../components/MultiSelect'
-	export default MultiSelect
+    import MultiSelect from './../../components/MultiSelect'
+    export default MultiSelect
 </script>
 <style>
 	.addBorder{
@@ -62,6 +61,32 @@
 	.selected{
 		margin-left: 3px;
 	}
+
+	.iconC::after{
+		display: inline-block;
+		font: normal normal normal 14px/1 FontAwesome;
+		text-rendering: auto;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		position: absolute;
+		display: block;
+		padding-top: 11px;
+		top: 1px;
+		right: 6px;
+		height: calc(100% - 2px);
+		width: 12px;
+		color: #74767d;
+		z-index: 2;
+		content: "\f107";
+		font-family: 'Font Awesome 5 Free';
+		font-weight: 600;
+		font-size: 13px;
+	}
+
+	.iconD::after{
+		transform: rotate(180deg);
+	}
+
 	.multi{
 		max-height: 145px !important;
 		height: 100% !important;
@@ -70,24 +95,24 @@
 
 	.iconC::after{
 		content: "\f078";
-        display: inline-block;
-        font: normal normal normal 14px/1 FontAwesome;
-        text-rendering: auto;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        position: absolute;
-        display: block;
-        padding-top: 11px;
-        top: 1px;
-        right: 6px;
-        height: calc(100% - 2px);
-        width: 12px;
-        font-size: 10px;
-        color: #74767d;
-        z-index: 2;
-        content: "\f078";
-        font-family: "Font Awesome 5 Free";
-        font-weight: 900;
+		display: inline-block;
+		font: normal normal normal 14px/1 FontAwesome;
+		text-rendering: auto;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		position: absolute;
+		display: block;
+		padding-top: 11px;
+		top: 1px;
+		right: 6px;
+		height: calc(100% - 2px);
+		width: 12px;
+		font-size: 10px;
+		color: #74767d;
+		z-index: 2;
+		content: "\f078";
+		font-family: "Font Awesome 5 Free";
+		font-weight: 900;
 	}
 	.iconD::after{
 		transform: rotate(180deg);
