7e239daec2a95bfba616af47278f84bb6a304556
Duan Nguyen <duan.nguyen@bigin.vn>
2018-11-28 15:46:43 +0700

    Update

--- src/themes/ios/Rating.vue
+++ src/themes/ios/Rating.vue
@@ -2,7 +2,7 @@
 	<div class="b__components b-ios b-rating">
 		<ul :class="classes">
 			<li v-for = "item in listRate" :class="item <= rate ? 'active' : '' " @click="update(item)">
-				<i class="fas fa-star" aria-hidden="true" v-show=" item > rate "></i>
+				<i class="far fa-star" aria-hidden="true" v-show=" item > rate "></i>
 				<i class="fas fa-star" aria-hidden="true" v-show=" item <= rate "></i>
 			</li>
 		</ul>
