7758795411da8a451fca6815c0a81cada8acc3c2
Duan Nguyen <duan.nguyen@bigin.vn>
2019-01-09 15:05:28 +0700

    Update

--- src/components/vue-tables-2/compiled/methods/init-pagination.js
+++ src/components/vue-tables-2/compiled/methods/init-pagination.js
@@ -5,10 +5,6 @@ module.exports = function () {
   this.page = 1;
 
   if (!this.opts.pagination.dropdown) {
-    if(this.searchPage != null && this.searchPage != undefined ){
-    	this.$refs.pagination.setPageHoang(parseInt(this.searchPage));
-  	}else{
-  		console.log('The "searchPage" must be fill out full !')
-  	}
+   	this.$refs.pagination.setPageHoang(parseInt(this.searchPage));
   }
 };
