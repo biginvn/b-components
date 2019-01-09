4e9e816b4ef7798bc6687a7956d400cdf881231a
Duan Nguyen <duan.nguyen@bigin.vn>
2019-01-09 14:47:39 +0700

    Update due table 2 search

--- src/components/vue-tables-2/compiled/methods/init-pagination.js
+++ src/components/vue-tables-2/compiled/methods/init-pagination.js
@@ -5,6 +5,10 @@ module.exports = function () {
   this.page = 1;
 
   if (!this.opts.pagination.dropdown) {
-    this.$refs.pagination.setPageHoang();
+    if(this.searchPage != null && this.searchPage != undefined ){
+    	this.$refs.pagination.setPageHoang(parseInt(this.searchPage));
+  	}else{
+  		console.log('The "searchPage" must be fill out full !')
+  	}
   }
 };
