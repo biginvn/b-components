3b26b1061fc4e6fb54031b8e817649272ac08fd1
AnhPhamBKIT <phamtuananh.bkit@gmail.com>
2019-06-26 12:05:16 +0700

    fix auto complete

--- src/components/BAutoComplete.js
+++ src/components/BAutoComplete.js
@@ -159,5 +159,49 @@ export default {
             this.updateFloatLabel(value);
             this.$emit('input', value);
         },
+        outside(e) {
+            let inputElement = $(this)[0].$el;
+            let elementTarget = e.target.parentElement;
+            if (inputElement !== elementTarget) {
+                if (!this.clickedChooseItem) {
+                    this.searchMatch = [];
+                    this.selectedIndex = -1;
+                }
+                this.clickedChooseItem = false;
+                this.resetFormatInputValue()
+            }
+        },
+    },
+    directives: {
+        'click-outside': {
+            bind: function(el, binding, vNode) {
+                // Provided expression must evaluate to a function.
+                if (typeof binding.value !== 'function') {
+                    const compName = vNode.context.name
+                    let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`
+                    if (compName) { warn += `Found in component '${compName}'` }
+
+                    console.warn(warn)
+                }
+                // Define Handler and cache it on the element
+                const bubble = binding.modifiers.bubble
+                const handler = (e) => {
+                    if (bubble || (!el.contains(e.target) && el !== e.target)) {
+                        binding.value(e)
+                    }
+                }
+                el.__vueClickOutside__ = handler
+
+                // add Event Listeners
+                document.addEventListener('click', handler)
+            },
+
+            unbind: function(el, binding) {
+                // Remove Event Listeners
+                document.removeEventListener('click', el.__vueClickOutside__)
+                el.__vueClickOutside__ = null
+
+            }
+        }
     }
 }
--- src/themes/ios/BAutoComplete.vue
+++ src/themes/ios/BAutoComplete.vue
@@ -3,11 +3,20 @@
         <label :class="classLabel">{{label}}</label>
         <textarea v-if="textarea" :splitCharacter="splitCharacter" :id="id" @blur="blur" @input="change($event.target.value)" :class="classes" :rows="rows" :cols="cols" class="autocomplete-input" :placeholder="placeholder" @focusout="focusout" @focus="focus" @keydown.13="chooseItem" @keydown.tab="chooseItem" @keydown.40="moveDown" @keydown.38="moveUp" v-model="inputValue"
                   type="text"></textarea>
-        <input v-else :id="id" @blur="blur" @input="change($event.target.value)" class="autocomplete-input" :class="classes" :placeholder="placeholder" @focusout="focusout" @focus="focus" @keydown.13="chooseItem" @keydown.tab="chooseItem" @keydown.40="moveDown" @keydown.38="moveUp" v-model="inputValue" type="text">
+        <input v-else :id="id"
+               @blur="blur"
+               @input="change($event.target.value)"
+               class="autocomplete-input" :class="classes" :placeholder="placeholder"
+               @focus="focus"
+               @keydown.13="chooseItem"
+               @keydown.tab="chooseItem"
+               @keydown.40="moveDown"
+               @keydown.38="moveUp"
+               v-model="inputValue" type="text">
         <ul :class="{
         'autocomplete-list': true,
         [id+'-list']: true
-      }" v-if="searchMatch.length > 0">
+      }" v-if="searchMatch.length > 0" v-click-outside="outside">
             <li :class="{active: selectedIndex === index}" v-for="(result, index) in searchMatch" @click="selectItem(index), chooseItem()" v-html="highlightWord(result)">
 
             </li>
