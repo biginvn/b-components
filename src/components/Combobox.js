e1c4afb789a07dfbd4ecabf532ed6c62f1807745
Trinh Le <trinh.le@bigin.vn>
2019-10-10 17:50:53 +0700

    update code

--- src/components/Combobox.js
+++ src/components/Combobox.js
@@ -260,10 +260,10 @@ export default {
         },
         toggleItem(id, index){
             this.selectedValue = id;
-            // this.selectedPointerIndex = index;
-            this.switchList(false); // Close list
-            this.searchKeyword = this.searchList[index].title;
-            // this.showResult = true
+            this.$nextTick(() => {
+                 this.switchList(false); // Close list
+                this.searchKeyword = this.searchList[index].title;
+            })
         },
         hoverItem(index){ // Hover on item at (index) in searchList
             // this
