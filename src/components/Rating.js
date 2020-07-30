43225a64395e86d885ac38b395381632facfaeca
Chien Tran <chien.tran@bigin.vn>
2020-07-30 10:05:17 +0700

    Update rating new design and round 0.5 star

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,37 +1,83 @@
 import baseComponent from '../mixins/base-mixins'
+
 export default {
-	mixins : [baseComponent],
-	props : [ 'disabled', 'name', 'id', 'label', 'class-name', 'rate-max','class-disabled' ],
-	computed : {
-		listRate () {
+	mixins: [baseComponent],
+	props: ['disabled', 'name', 'id', 'label', 'class-name', 'rate-max', 'class-disabled'],
+	computed: {
+		/**
+		 * get list rate
+		 * @return {[]|number[]}
+		 */
+		listRate() {
 			if (this.rateMax == undefined || this.rateMax == null)
-				return [1,2,3,4,5]
+				return [1, 2, 3, 4, 5]
 
 			let list = []
-			for(let i=1; i <= parseInt(this.rateMax); i++)
+			for (let i = 1; i <= parseInt(this.rateMax); i++)
 				list.push(i)
 			return list
 		},
-		rate () {
+		/**
+		 * get rating value
+		 * @return {number}
+		 */
+		rate() {
 			if (this.value == undefined || this.value == null)
 				return 0;
-			else return parseInt(this.value)
+			else return Math.round(this.value * 2) / 2;
 		},
-		classes () {
-			let classStar = (this.className ? this.className : '')
-			classStar += this.rate >= this.listRate.length ? ' full' : ''
+		/**
+		 * get class config
+		 * @return {string}
+		 */
+		classes() {
+			let classStar = this.className ? this.className : ''
 			return classStar + ' rating__input'
 		},
-		classDis (){
-			let disabled = (this.classDisabled ? this.classDisabled : '')
-			disabled += this.rate >= this.listRate.length ? ' full' : ''
+		/**
+		 * get class when disabled
+		 * @return {computed.classDisabled|string}
+		 */
+		classDis() {
+			let disabled = this.classDisabled ? this.classDisabled : ''
+			disabled += this.isDisabled ? ' disabled-rating' : ''
 			return disabled
+		},
+		/**
+		 * check is disabled
+		 * @return {boolean}
+		 */
+		isDisabled() {
+			return this.disabled == 'disabled'
 		}
 	},
-	methods : {
+	methods: {
+		/**
+		 * function update rating when filled
+		 * @param rate
+		 */
 		update(rate) {
-			if (this.disabled != 'disabled')
+			if (!this.isDisabled)
 				this.$emit('input', rate)
+		},
+		/**
+		 * get icon class star base on number star
+		 * @param item
+		 * @return {string}
+		 */
+		classStar(item) {
+			if (Math.ceil(this.rate) == item && this.isFloat(this.rate)) {
+				return 'fa fa-star-half-alt';
+			}
+			return item > this.rate ? 'far fa-star' : 'fas fa-star'
+		},
+		/**
+		 * check number is float
+		 * @param number
+		 * @return {boolean}
+		 */
+		isFloat(number) {
+			return Number(number) !== parseInt(number);
 		}
 	}
 
--- src/themes/ios/Rating.vue
+++ src/themes/ios/Rating.vue
@@ -1,14 +1,19 @@
 <template>
 	<div class="b__components b-ios b-rating" :class="classDis">
 		<ul :class="classes">
-			<li v-for = "item in listRate" :class="item <= rate ? 'active' : '' " @click="update(item)">
-				<i class="far fa-star" aria-hidden="true" v-show=" item > rate "></i>
-				<i class="fas fa-star" aria-hidden="true" v-show=" item <= rate "></i>
+			<li v-for="item in listRate" class="active" @click="update(item)">
+				<i :class="classStar(item)" aria-hidden="true"></i>
 			</li>
 		</ul>
 	</div>
 </template>
 <script>
 	import Rating from './../../components/Rating'
+
 	export default Rating
 </script>
+<style scoped>
+	.disabled-rating {
+		pointer-events: none;
+	}
+</style>
