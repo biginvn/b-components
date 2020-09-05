f09d2ac4c4729f2d7932d6133b42da9f9758cce8
Trinh Le <trinh.le@bigin.vn>
2020-09-06 01:15:24 +0700

    update component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,77 +1,95 @@
 import baseComponent from '../mixins/text-field-mixins'
 
 export default {
-	mixins : [baseComponent],
-	// props	: ['type', 'min', 'maxlength', 'classParent', 'regex', 'url', 'has-url', 'target-blank'],
-    props : {
-        type: {},
-        min: {},
-        maxlength: {},
-        classParent: {},
-        regex: {},
-		url: {},
-		autocomplete:{},
+    mixins: [baseComponent],
+    props: {
+        type: {
+            type: String,
+            default: 'text',
+        },
+        min: {
+            type: Number / String,
+            default: null,
+        },
+        maxlength: {
+            type: Number / String,
+            default: null,
+        },
+        classParent: {
+            type: String,
+            default: '',
+        },
+        regex: {
+            default: ''
+        },
+        url: {
+            type: String,
+            default: '',
+        },
+        autocomplete: {
+            type: String,
+            default: '',
+        },
         hasUrl: {
             default: false
-		},
-		targetBlank: {
+        },
+        targetBlank: {
             default: true
-		},
-		negative: {
-			default: true
-		}
-    },
-	computed : {
-		classes () {
-			return (this.className?this.className:'') + " b__input 2"
-		},
-		typeComponent () {
-			if (this.type == undefined || this.type== null || this.type.length == 0)
-				return 'text'
-			return this.type
-		},
-		classesParent() {
- 		 	 return (this.classParent?this.classParent:'')
-		},
-        hasLinkView () {
-			if (this.hasUrl && this.disabled) return true;
-			return false
+        },
+        negative: {
+            default: true
         }
-	},
-	methods : {
-		change (value) {
-			this.updateFloatLabel(value);
-			this.$emit('input', value);
-		},
-		blur() {
-			this.$emit('blur')
-		},
-		focus() {
-			this.$emit('focus')
-		},
-		// keyup(value){
-		// 	this.$emit('keyup', value)
-		// },
-		maxLength(){
-		   if (this.maxlength == undefined || this.maxlength== null)
-		    return
-		   return this.maxlength
-		},
-		keyupEnter: function(e) {
-			var keyCode = e.keyCode || e.which;
-     		  //if (keyCode === 13 || keyCode === 45) {
+    },
+    computed: {
+        /**
+         * @inheritDoc
+         */
+        classes() {
+            return `${this.className || ''} b__input 2`
+        },
+        /**
+         * @inheritDoc
+         */
+        hasLinkView() {
+            return this.hasUrl && this.disabled
+        }
+    },
+    methods: {
+        /**
+         * @inheritDoc
+         */
+        updateFloatLabel(value) {
+            const isEmpty = value == undefined || value == null || value == '' ? true : false;
+            this.classLabel = isEmpty ? '' : 'active'
+        },
+        /**
+         * @inheritDoc
+         */
+        change(value) {
+            this.updateFloatLabel(value);
+            this.$emit('input', value);
+        },
+        /**
+         * @inheritDoc
+         */
+        keyupEnter: function(e) {
+            const keyCode = e.keyCode || e.which;
             if (keyCode === 13) {
-     		   	e.preventDefault();
+                e.preventDefault();
                 this.$emit('keyup-enter');
-     		   	return false;
-     		}   
-   		},
-        keyDownRegex: function (e) {
-        	let keyCode = e.keyCode || e.which;
+                return false;
+            }
+        },
+        /**
+         * @inheritDoc
+         */
+        keyDownRegex: function(e) {
+            const keyCode = e.keyCode || e.which;
 
-        	let pattern = new RegExp(this.regex);
-            if (this.regex !== undefined && this.regex !== null && this.regex !== '') {
-            	const value = $(e.target).val() + e.key
+            let pattern = new RegExp(this.regex);
+            if (!!this.regex) {
+
+                const value = $(e.target).val() + e.key
                 let res = pattern.test(value);
                 if (!res && keyCode != 8 && keyCode != 37 && keyCode != 39) {
                     e.preventDefault();
@@ -80,9 +98,9 @@ export default {
             }
 
             // Don't validate the input if below arrow, delete and backspace keys were pressed
-            if(keyCode != 37 && keyCode != 38 && keyCode != 39 && keyCode != 40 && keyCode != 46 && keyCode != 8) { // Left / Up / Right / Down Arrow, Delete keys;
+            if (keyCode != 37 && keyCode != 38 && keyCode != 39 && keyCode != 40 && keyCode != 46 && keyCode != 8) { // Left / Up / Right / Down Arrow, Delete keys;
                 let keyCharacter = e.key;
-                if (this.regex !== undefined && this.regex !== null && this.regex !== '') {
+                if (!!this.regex) {
                     let res = pattern.test(keyCharacter);
                     if (!res) {
                         e.preventDefault();
@@ -91,5 +109,5 @@ export default {
                 }
             }
         }
-	}
+    }
 }
