4ae4b108cb4339f6550d02f7b54048e3d632f691
Trinh Le <trinh.le@bigin.vn>
2020-09-23 23:35:19 +0700

    fix table sort icon

--- src/components/vue-tables-2/compiled/config/defaults.js
+++ src/components/vue-tables-2/compiled/config/defaults.js
@@ -1,104 +1,104 @@
 "use strict";
 
-module.exports = function () {
-  return {
-    dateColumns: [],
-    listColumns: {},
-    datepickerOptions: {
-      locale: {
-        cancelLabel: 'Clear'
-      }
-    },
-    datepickerPerColumnOptions: {},
-    initialPage: 1,
-    perPage: 10,
-    perPageValues: [10, 25, 50, 100],
-    groupBy: false,
-    params: {},
-    sortable: true,
-    filterable: true,
-    groupMeta: [],
-    initFilters: {},
-    customFilters: [],
-    templates: {},
-    debounce: 250,
-    dateFormat: "DD/MM/YYYY",
-    dateFormatPerColumn: {},
-    toMomentFormat: false,
-    skin: false,
-    columnsDisplay: {},
-    columnsDropdown: false,
-    texts: {
-      count: "Showing {from} to {to} of {count} records|{count} records|1 record",
-      first: 'First',
-      last: 'Last',
-      filter: "Search:",
-      filterPlaceholder: "Search query",
-      limit: "Show",
-      entries: " entries",
-      page: "Page:",
-      noResults: "No matching records",
-      filterBy: "Filter by {column}",
-      loading: 'No matching records',
-      defaultOption: 'Select {column}',
-      columns: 'Columns'
-    },
-    sortIcon: {
-      is: 'fa-sort sort-btable',
-      base: 'fa sorting',
-      up: 'fa-sort-asc',
-      down: 'fa-sort-desc'
-    },
-    sortingAlgorithm: function sortingAlgorithm(data, column) {
-      return data.sort(this.getSortFn(column));
-    },
+module.exports = function() {
+    return {
+        dateColumns: [],
+        listColumns: {},
+        datepickerOptions: {
+            locale: {
+                cancelLabel: 'Clear'
+            }
+        },
+        datepickerPerColumnOptions: {},
+        initialPage: 1,
+        perPage: 10,
+        perPageValues: [10, 25, 50, 100],
+        groupBy: false,
+        params: {},
+        sortable: true,
+        filterable: true,
+        groupMeta: [],
+        initFilters: {},
+        customFilters: [],
+        templates: {},
+        debounce: 250,
+        dateFormat: "DD/MM/YYYY",
+        dateFormatPerColumn: {},
+        toMomentFormat: false,
+        skin: false,
+        columnsDisplay: {},
+        columnsDropdown: false,
+        texts: {
+            count: "Showing {from} to {to} of {count} records|{count} records|1 record",
+            first: 'First',
+            last: 'Last',
+            filter: "Search:",
+            filterPlaceholder: "Search query",
+            limit: "Show",
+            entries: " entries",
+            page: "Page:",
+            noResults: "No matching records",
+            filterBy: "Filter by {column}",
+            loading: 'No matching records',
+            defaultOption: 'Select {column}',
+            columns: 'Columns'
+        },
+        sortIcon: {
+            is: 'fa-sort sort-btable',
+            base: 'fa sorting',
+            up: 'fa-sort-up',
+            down: 'fa-sort-down'
+        },
+        sortingAlgorithm: function sortingAlgorithm(data, column) {
+            return data.sort(this.getSortFn(column));
+        },
 
-    customSorting: {},
-    multiSorting: {},
-    clientMultiSorting: true,
-    serverMultiSorting: false,
-    filterByColumn: false,
-    highlightMatches: false,
-    orderBy: false,
-    descOrderColumns: [],
-    footerHeadings: false,
-    headings: {},
-    headingsTooltips: {},
-    pagination: {
-      dropdown: false,
-      chunk: 10,
-      edge: false,
-      align: 'left',
-      nav: 'fixed'
-    },
-    childRow: false,
-    childRowTogglerFirst: true,
-    uniqueKey: 'id',
-    requestFunction: false,
-    requestAdapter: function requestAdapter(data) {
-      return data;
-    },
-    responseAdapter: function responseAdapter(resp) {
+        customSorting: {},
+        multiSorting: {},
+        clientMultiSorting: true,
+        serverMultiSorting: false,
+        filterByColumn: false,
+        highlightMatches: false,
+        orderBy: false,
+        descOrderColumns: [],
+        footerHeadings: false,
+        headings: {},
+        headingsTooltips: {},
+        pagination: {
+            dropdown: false,
+            chunk: 10,
+            edge: false,
+            align: 'left',
+            nav: 'fixed'
+        },
+        childRow: false,
+        childRowTogglerFirst: true,
+        uniqueKey: 'id',
+        requestFunction: false,
+        requestAdapter: function requestAdapter(data) {
+            return data;
+        },
+        responseAdapter: function responseAdapter(resp) {
 
-      var data = this.getResponseData(resp);
+            var data = this.getResponseData(resp);
 
-      return {
-        data: data.data,
-        count: data.count
-      };
-    },
-    requestKeys: {
-      query: 'query',
-      limit: 'limit',
-      orderBy: 'orderBy',
-      ascending: 'ascending',
-      page: 'page',
-      byColumn: 'byColumn'
-    },
-    rowClassCallback: false,
-    preserveState: false,
-    saveState: false,
-    storage: 'local',
-    columnsClasses: {}
-  };
+            return {
+                data: data.data,
+                count: data.count
+            };
+        },
+        requestKeys: {
+            query: 'query',
+            limit: 'limit',
+            orderBy: 'orderBy',
+            ascending: 'ascending',
+            page: 'page',
+            byColumn: 'byColumn'
+        },
+        rowClassCallback: false,
+        preserveState: false,
+        saveState: false,
+        storage: 'local',
+        columnsClasses: {}
+    };
 };
