ade4492afbb1ea43a2e51773b637f07894e5c2c0
Trinh Le <trinh.le@bigin.vn>
2020-09-23 23:55:00 +0700

    add auto complete

--- src/components/vue-tables-2/compiled/modules/normal-filter.js
+++ src/components/vue-tables-2/compiled/modules/normal-filter.js
@@ -2,18 +2,20 @@
 
 var debounce = require('debounce');
 
-module.exports = function (h) {
+module.exports = function(h) {
     var _this = this;
 
-    return function (classes, id) {
+    return function(classes, id) {
 
         var search = _this.source == 'client' ? _this.search.bind(_this, _this.data) : _this.serverSearch.bind(_this);
 
         return h(
-            'input',
-            { 'class': classes.input + ' ' + classes.small,
-                attrs: { type: 'text',
+            'input', {
+                'class': classes.input + ' ' + classes.small,
+                attrs: {
+                    type: 'text',
                     value: _this.query,
+                    autocomplete: 'new-password',
                     // placeholder: _this.display('filterPlaceholder'),
                     id: id
                 },
